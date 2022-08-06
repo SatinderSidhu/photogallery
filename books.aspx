46275294414679f78b72936e0c3aee92e7f01424
Satinder Sidhu <satinder.satty@gmail.com>
2022-08-06 16:10:03 -0400

    FIRST COMMIT

---
+++ books.aspx
@@ -0,0 +1,40 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="books.aspx.cs" Inherits="WebApplication3.books" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+
+            <div style="float:left; display:block">
+                <asp:Literal runat="server" ID="divImages"></asp:Literal>
+            </div>            
+            <asp:DataList runat="server" ID="DL_Writers" RepeatDirection="Vertical" RepeatColumns="4">
+                <ItemStyle VerticalAlign="Top" HorizontalAlign="Center" />
+                <ItemTemplate>
+                    <div style="padding: 10px 10px 5px 10px;">
+                        <asp:Image ImageUrl='<%# DataBinder.Eval(Container.DataItem, "IMageUrl").ToString() %>'
+                            runat="server" ID="ImgProfile" Style="border: 3px solid grey; max-height: 250px" />
+                    </div>
+
+                </ItemTemplate>
+            </asp:DataList>
+
+            <asp:GridView ID="GridBooks" runat="server" AutoGenerateColumns="false" Visible="false">
+                <Columns>
+                    <asp:TemplateField HeaderText="Preview Image">
+                        <ItemTemplate>
+                            <asp:Image ID="ImageButton1" runat="server" ImageUrl='<%# Eval("ImageUrl")%>'
+                                Height="250px" Style="cursor: pointer" />
+                        </ItemTemplate>
+                    </asp:TemplateField>
+                </Columns>
+            </asp:GridView>
+        </div>
+    </form>
+</body>
+</html>
---
+++ writers.aspx.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication3
+{
+    public partial class writers : System.Web.UI.Page
+    {
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string PHOTO_FOLDER_NAME = "galleryimages/writers/";
+            string PHOTO_HEIGHT = "300px";
+
+            if (!IsPostBack)
+            {
+                string[] filePaths = Directory.GetFiles(Server.MapPath(PHOTO_FOLDER_NAME));
+
+
+                DataTable dt = new DataTable();
+                dt.Columns.AddRange(new DataColumn[2] {
+                    new DataColumn("ImagerName", typeof(string)),
+                    new DataColumn("ImageUrl",typeof(string)) });
+
+
+
+                List<ListItem> files = new List<ListItem>();
+                int imageCount = 0;
+
+                var divHtml = new System.Text.StringBuilder();
+                foreach (string filePath in filePaths)
+                {
+                    imageCount++;
+                    string fileName = Path.GetFileName(filePath);
+                    files.Add(new ListItem(fileName, PHOTO_FOLDER_NAME + fileName));
+                    dt.Rows.Add(fileName, PHOTO_FOLDER_NAME + fileName);
+
+                    divHtml.Append("<div ");
+                    divHtml.Append(" style='padding:10px;float:left' id=id" + imageCount.ToString() + ">");
+                    divHtml.Append("<img height='" + PHOTO_HEIGHT + "' id=img" + imageCount.ToString() + " src='" + PHOTO_FOLDER_NAME + fileName + "'>");
+                    divHtml.Append(" <br>    PBID # " + imageCount.ToString() + "</div>");
+                    divHtml.Append("</div>");
+
+                }
+                divImages.Text = divHtml.ToString();
+                //GridBooks.DataSource = dt;
+
+                //GridBooks.DataBind();
+
+                //DL_Writers.DataSource = dt;
+                //DL_Writers.DataBind();
+            }
+        }
+    }
+}
